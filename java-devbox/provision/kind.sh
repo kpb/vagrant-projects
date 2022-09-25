ca4b0afda4a5e007185ba7540a2e15c57ccc6bbf
Kenneth Bowen <kenneth@kennethbowen.com>
2022-09-25 08:38:32 -0600

    Setup KinD completion

--- java-devbox/provision/kind.sh
+++ java-devbox/provision/kind.sh
@@ -8,3 +8,5 @@ echo "Installing KinD version ${kind_version}"
 
 curl -sLo /usr/local/bin/kind https://kind.sigs.k8s.io/dl/${kind_version}/kind-linux-amd64
 chmod 755 /usr/local/bin/kind
+
+kind completion bash >/etc/bash_completion.d/kind
